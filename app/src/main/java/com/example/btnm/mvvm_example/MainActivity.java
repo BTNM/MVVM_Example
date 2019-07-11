d88588fb3169f817b6fac86ed6c6c586cc9b2b31
BaoTNM <BaoTNguyenM@gmail.com>
2019-07-11 20:06:42 +0200

    Initial commit, changed dependencies from old builds to androidx dependencies

---
+++ app/src/main/java/com/example/btnm/mvvm_example/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.btnm.mvvm_example;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
